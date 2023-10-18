55b693a5ba0e4879c30a70433c273f4d7a3a91d9
Akechi001 <enricoarianthous@gmail.com>
2023-10-18 17:52:22 +0800

    done

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -14,6 +14,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'colors.dart ';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -23,6 +25,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,13 +39,91 @@ class _LoginPageState extends State<LoginPage> {
               children: <Widget>[
                 Image.asset('assets/diamond.png'),
                 const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Text(
+                  'SHRINE',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
               ],
             ),
-            const SizedBox(height: 120.0),
+            const SizedBox(height: 12.0),
             // TODO: Remove filled: true values (103)
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                // Removed filled: true
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                // Removed filled: true
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
             // TODO: Add TextField widgets (101)
+            // TextField(
+            //   controller: _usernameController,
+            //   decoration: InputDecoration(
+            //     filled: true,
+            //     labelText: 'Username',
+            //   ),
+            // ),
+// spacer
+//             const SizedBox(height: 12.0),
+// // [Password]
+//             TextField(
+//               controller: _passwordController,
+//               decoration: InputDecoration(
+//                 filled: true,
+//                 labelText: 'Password',
+//               ),
+//               obscureText: true,
+//             ),
             // TODO: Add button bar (101)
+            const SizedBox(height: 12.0),
+             OverflowBar(
+              alignment: MainAxisAlignment.end,
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // TODO: Add buttons (101)
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                  style: TextButton.styleFrom(
+
+                    foregroundColor: kShrineBrown900,
+                    shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    ),
+
+                    primary: Theme.of(context).colorScheme.secondary,
+                  ),
+                ),
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: kShrineBrown900,
+                    backgroundColor: kShrinePink100,
+                    elevation: 8.0,
+                    shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
           ],
         ),
       ),
